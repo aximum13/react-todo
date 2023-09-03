63ea1178cd3cd08d7b50706f752e98e34e798b17
aximum13 <aximum2@yandex.ru>
2023-09-04 01:48:12 +0300

    Add todo app (noRedux)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,132 @@
-import logo from './logo.svg';
-import './App.css';
+import classNames from "classnames";
+import React, { useEffect, useState } from "react";
+import { TodoNew } from "./componets/Header/TodoNew";
+import { Title } from "./componets/Header/Title";
+import { TodoList } from "./componets/List/TodoList";
+import { Filter } from "./componets/Footer/Filter";
 
-function App() {
+import "./styles/style.css";
+
+const App = () => {
+  const [item, setItem] = useState("");
+  const [items, setItems] = useState(
+    JSON.parse(localStorage.getItem("items")) || []
+  );
+  const [filtered, setFiltered] = useState(items);
+  let [checked, setChecked] = useState(true);
+
+  if (items.every((item) => item.done === true)) {
+    checked = true;
+  } else {
+    checked = false;
+  }
+
+  const createTodo = (newTodo) => {
+    setItems([...items, newTodo]);
+    setItem("");
+    setFiltered([...filtered, newTodo]);
+  };
+
+  const updateStateTodo = (todo) => {
+    const updateStateTodo = items.map((item) =>
+      item.id === todo.id ? { ...item, done: !item.done } : { ...item }
+    );
+
+    setItems(updateStateTodo);
+    setFiltered(updateStateTodo);
+  };
+
+  const updateTodo = (todos) => {
+    setItems(todos);
+    setFiltered(todos);
+  };
+
+  const filterTodos = (state) => {
+    if (state === "all") setFiltered(items);
+    else {
+      let newTodos = [...items].filter((item) => item.done === state);
+      setFiltered(newTodos);
+    }
+  };
+
+  const removeTodo = (todo) => {
+    const updateItems = filtered.filter((item) => item.id !== todo.id);
+    setFiltered(updateItems);
+    setItems(updateItems);
+  };
+
+  const clearCompleteTodos = () => {
+    const updateItems = filtered.filter((item) => item.done === false);
+    setFiltered(updateItems);
+    setItems(updateItems);
+  };
+
+  const allTodosDone = () => {
+    let updatedItems = items.map((item) => ({
+      ...item,
+      done: !checked,
+    }));
+
+    setChecked(!checked);
+
+    if (updatedItems.every((item) => item.done === true)) {
+      setChecked(!checked);
+    }
+
+    setItems(updatedItems);
+    setFiltered(updatedItems);
+  };
+
+  const activeTodos = () => {
+    let countTodos = items.filter((item) => item.done === false);
+    return countTodos.length;
+  };
+
+  const handleChange = () => {
+    setChecked(!checked);
+  };
+
+  useEffect(() => {
+    localStorage.setItem("items", JSON.stringify(items));
+  }, [items]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todoapp">
+      <div className="header">
+        <Title title={"todos"} />
+        <TodoNew create={createTodo} />
+      </div>
+      <div className="main">
+        <input
+          id="toggle-all"
+          onClick={allTodosDone}
+          onChange={handleChange}
+          checked={checked}
+          className={classNames(
+            "toggle-all",
+            items.length === 0 ? "hidden" : ""
+          )}
+          type="checkbox"
+        />
+        <label htmlFor="toggle-all">Mark all as complete</label>
+        <TodoList
+          remove={removeTodo}
+          list={filtered}
+          updateState={updateStateTodo}
+          edit={updateTodo}
+        />
+      </div>
+      <div className={classNames("footer", items.length === 0 ? "hidden" : "")}>
+        <span className="todo-count">
+          <strong>{activeTodos()}</strong> item left
+        </span>
+        <Filter todoFilter={filterTodos} />
+        <button onClick={clearCompleteTodos} className="clear-completed">
+          Clear completed
+        </button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/componets/Footer/TodoCount.jsx
@@ -0,0 +1,3 @@
+export const TodoCount = ({ title }) => {
+  <h1>{title}</h1>;
+};
